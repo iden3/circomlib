d1f0aafcffcd8aafa25c0c44e0f4c5a143a8bee2
Kobi Gurkan <kobigurk@gmail.com>
2019-07-07 18:13:36 +0300

    feat: exposes pedersen hash

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.eddsa = require("./src/eddsa");
 exports.mimc7 = require("./src/mimc7");
 exports.mimcsponge = require("./src/mimcsponge");
 exports.babyJub = require("./src/babyjub");
+exports.pedersenHash = require("./src/pedersenHash");
