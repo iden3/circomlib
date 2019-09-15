e3cd0e5aa79a750b08cd6f62cd233e68c9758faf
Jordi Baylina <jordi@baylina.cat>
2019-09-15 10:52:18 +0200

    Dependencies and publish smt

--- src/smt.js
+++ src/smt.js
@@ -310,3 +310,4 @@ async function newMemEmptyTrie() {
 module.exports.loadFromFile = loadFromFile;
 module.exports.newMemEmptyTrie = newMemEmptyTrie;
 module.exports.SMT = SMT;
+module.exports.SMTMemDB = SMTMemDB;
