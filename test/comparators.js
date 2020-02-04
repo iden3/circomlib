cf6d1f0bb16b141891ed52aef890661b32653a0c
Jordi Baylina <jordi@baylina.cat>
2020-02-04 19:19:11 +0100

    Test name changed on comparators

--- test/comparators.js
+++ test/comparators.js
@@ -7,7 +7,7 @@ const bigInt = require("big-integer");
 
 const assert = chai.assert;
 
-describe("Sum test", function ()  {
+describe("Comparators test", function ()  {
 
     this.timeout(100000);
 
