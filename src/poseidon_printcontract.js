01e3f0d6807137fb01c085b88cfaf63661e4a946
Jordi Baylina <jordi@baylina.cat>
2020-08-09 17:19:53 +0200

    Poseidon genContract nInputs as parameters

--- src/poseidon_printcontract.js
+++ src/poseidon_printcontract.js
@@ -1,5 +1,13 @@
 const poseidonGenContract = require("./poseidon_gencontract");
 
+if (process.argv.length != 3) {
+    console.log("Usage: node poseidon_gencontract.js [numberOfInputs]");
+    process.exit(1);
+}
 
-console.log(poseidonGenContract.createCode(5));
+const nInputs = Number(process.argv[2]);
+
+console.log(nInputs);
+
+console.log(poseidonGenContract.createCode(nInputs));
 
