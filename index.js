5a523a43c25071a0425746b635619c06fbd69cf5
Jordi Baylina <jordi@baylina.cat>
2021-10-23 16:38:57 +0200

    Clean index.js

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-exports.smt = require("./src/smt");
-exports.eddsa = require("./src/eddsa");
-exports.mimc7 = require("./src/mimc7");
-exports.mimcsponge = require("./src/mimcsponge");
-exports.babyJub = require("./src/babyjub");
-exports.pedersenHash = require("./src/pedersenHash");
-exports.SMT = require("./src/smt").SMT;
-exports.SMTMemDB = require("./src/smt_memdb");
-exports.poseidon = require("./src/poseidon");
-
 
 
